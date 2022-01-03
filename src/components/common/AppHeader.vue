b3d8ac4a7dd02e1637404feb215e294887af05f2
Jack <79830954+Captainjack-kor@users.noreply.github.com>
2022-01-03 17:25:57 +0900

    Initialize 3_signup

---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <router-link to="/login">로그인</router-link> |
+    <router-link to="/signup">회원가입</router-link>
+    <div>{{ test }}</div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: process.env.VUE_APP_API_VALUE,
+    };
+  },
+};
+</script>
+
+<style></style>
